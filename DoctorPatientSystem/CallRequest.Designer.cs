2db2d169443d4c2f365166eef2bec31624ade6a0
alexdixon41 <34317910+alexdixon41@users.noreply.github.com>
2018-11-28 20:01:59 -0500

    GUI improvements

--- DoctorPatientSystem/CallRequest.Designer.cs
+++ DoctorPatientSystem/CallRequest.Designer.cs
@@ -36,38 +36,46 @@
             // 
             // doctorListBox
             // 
+            this.doctorListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.doctorListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.doctorListBox.FormattingEnabled = true;
             this.doctorListBox.ItemHeight = 20;
-            this.doctorListBox.Location = new System.Drawing.Point(18, 100);
+            this.doctorListBox.Location = new System.Drawing.Point(27, 105);
+            this.doctorListBox.Margin = new System.Windows.Forms.Padding(3, 3, 20, 3);
             this.doctorListBox.Name = "doctorListBox";
-            this.doctorListBox.Size = new System.Drawing.Size(462, 224);
+            this.doctorListBox.Size = new System.Drawing.Size(796, 584);
             this.doctorListBox.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(246, 29);
+            this.label1.Size = new System.Drawing.Size(374, 51);
             this.label1.TabIndex = 6;
             this.label1.Text = "Request a Phone Call";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 80);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 82);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 17);
+            this.label2.Size = new System.Drawing.Size(101, 19);
             this.label2.TabIndex = 7;
             this.label2.Text = "Select a Doctor";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(308, 331);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(549, 14);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(171, 34);
+            this.button1.Size = new System.Drawing.Size(266, 48);
             this.button1.TabIndex = 8;
             this.button1.Text = "Request Phone Call";
             this.button1.UseVisualStyleBackColor = true;
@@ -77,13 +85,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(832, 453);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.doctorListBox);
             this.Name = "CallRequest";
-            this.Text = "CallRequest";
+            this.Size = new System.Drawing.Size(835, 701);
             this.Load += new System.EventHandler(this.CallRequest_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
