214f2120d6571a8265974508166b66a36e0297b6
ethenholzapfel <eholzapfel_5@hotmail.com>
2018-12-04 21:23:23 -0500

    squashed a bug

--- DoctorPatientSystem/PatientSearch.Designer.cs
+++ DoctorPatientSystem/PatientSearch.Designer.cs
@@ -121,7 +121,7 @@
             this.backToPatientSearchButton = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-            this.label11 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
             this.patientView = new System.Windows.Forms.ListView();
             this.fName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.DOB = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -129,8 +129,7 @@
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
-            this.label14 = new System.Windows.Forms.Label();
-            this.patientBox.SuspendLayout();
+            this.label14 = new System.Windows.Forms.Label();            
             this.patientSearchPanel.SuspendLayout();
             this.viewMedicalRecordPanel.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -203,7 +202,7 @@
             // patientSearchPanel
             // 
             this.patientSearchPanel.Controls.Add(this.patientView);
-            this.patientSearchPanel.Controls.Add(this.label11);
+            this.patientSearchPanel.Controls.Add(this.label15);
             this.patientSearchPanel.Controls.Add(this.viewPatientInformation);
             this.patientSearchPanel.Controls.Add(this.nameSearch);
             this.patientSearchPanel.Controls.Add(this.patientSearchTextBox);
@@ -1302,13 +1301,13 @@
             // 
             // label11
             // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(19, 16);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(260, 51);
-            this.label11.TabIndex = 7;
-            this.label11.Text = "Patient Search";
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(19, 16);
+            this.label15.Name = "label11";
+            this.label15.Size = new System.Drawing.Size(260, 51);
+            this.label15.TabIndex = 7;
+            this.label15.Text = "Patient Search";
             // 
             // patientView
             // 
@@ -1537,7 +1536,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
-        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label15;
         private System.Windows.Forms.ListView patientView;
         private System.Windows.Forms.ColumnHeader fName;
         private System.Windows.Forms.ColumnHeader DOB;
